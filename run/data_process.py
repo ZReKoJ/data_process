fa7b30b7a9887be00a472debbd6c3ae49cb4b885
ZReKoJ <zhong.9745@gmail.com>
2025-02-06 22:33:46 +0100

    add parser

---
+++ run/data_process.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+def parse_arguments():
+    # Create argument parser
+    parser = argparse.ArgumentParser()
+
+    # Positional mandatory arguments
+    parser.add_argument("file", help="Flow Configuration file", type=str)
+
+    # Optional arguments with parameter
+    parser.add_argument("-i", "--id", type=str, help="Execution ID", action="append", default=[get_time(ateformat="%Y%m%d%H%M%S")])
+    
+    # Optional arguments without parameter
+    parser.add_argument("--show-command", help="Show the commands to execute the components", action="store_true")
+
+    # Others
+    parser.add_argument("--version", help="Check Version", action="version", version='%(prog)s - Version 1.0')
+
+    # Parse arguments
+    args = vars(parser.parse_args())
+    args["id"] = "_".join(args.get("id"))
+
+    return args
+
+if __name__ == '__main__': 
+
+    args = parse_arguments()
+
+
+
+
+

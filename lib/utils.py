3cba770af00a9bf1def9568aa18799bc1677f35d
Zihao Hong <zhrekoj@gmail.com>
2025-04-16 15:09:50 +0200

    Update utils.py progress_bar

--- lib/utils.py
+++ lib/utils.py
@@ -15,7 +15,7 @@ def progress_bar(current, total, prefix="Progress", fill="█", length=40):
     percent = ("{0:.1f}").format(100 * (current / float(total)))
     filled_length = int(length * current // total)
     bar = fill * filled_length + '-' * (length - filled_length)
-    print("{}: {} {}%".format(prefix, bar, percent))
+    return "{}: {} {}%".format(prefix, bar, percent)
 
 ######################## DATES
 

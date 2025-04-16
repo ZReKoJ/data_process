bf15c2d1d2308a61e202c5e971b7902e57ef7731
ZReKoJ <zhrekoj@gmail.com>
2025-04-16 12:16:56 +0200

    make tar_gz component be async

--- component/tar_gz.py
+++ component/tar_gz.py
@@ -5,12 +5,14 @@ import os
 import time
 import tarfile
 
+import concurrent.futures
+
 # Add the lib directory to the sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib')))
 
-from data_process_lib import Component
+from data_process_lib import AsyncComponent
 
-class TarGzComponent(Component):
+class TarGzComponent(AsyncComponent):
 
     def __init__(self):
         super().__init__()
@@ -33,12 +35,23 @@ class TarGzComponent(Component):
 
         self.log_info("Start Process")
 
-        for filepath in self._data:
-            basename = os.path.basename(os.path.normpath(filepath))
-            with tarfile.open(os.path.join(self._OUTPUT_PATH, basename + ".tar.gz"), "w:gz") as tar:
-                tar.add(filepath, arcname=basename)
+        futures = [ 
+            self._executor.submit(self.create_tar_gz_from_folder, filepath, self._OUTPUT_PATH) 
+            for filepath 
+            in self._data
+        ]
+        
+        for future in concurrent.futures.as_completed(futures):
+            # Just to trigger the exception if happens
+            future.result()
             
         self.log_info("End Process")
+
+    @classmethod
+    def create_tar_gz_from_folder(cls, source, destination):
+        basename = os.path.basename(os.path.normpath(source))
+        with tarfile.open(os.path.join(destination, basename + ".tar.gz"), "w:gz") as tar:
+            tar.add(source, arcname=basename)
 
 if __name__ == "__main__":
     try:

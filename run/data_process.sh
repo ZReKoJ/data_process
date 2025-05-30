f22fdf2a0e86b956139a374b6e4509914583abf2
Zihao Hong <zhrekoj@gmail.com>
2025-05-30 15:15:01 +0200

    Update data_process.sh

--- run/data_process.sh
+++ run/data_process.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-
 directory=$(dirname $(realpath $0))
 
-find ${directory}/../component -type f -exec dos2unix {} \;
-find ${directory}/../component -type f -exec chmod +x {} \;
+find ${directory}/../component -type f -exec dos2unix {} \; >/dev/null 2>&1
+find ${directory}/../component -type f -exec chmod +x {} \; >/dev/null 2>&1
 
-${directory}/../main.py $@
+if [ $# -eq 0 ]; then
+    path=${directory}/../flow
+    while ! [[ -f "$path" && "$path" == *.json ]]; do
+        select option in $(find ${path} -mindepth 1 -maxdepth 1 \( -type f -name '*.json' -o -type d \) -exec basename {} \;);
+        do
+            path="${path}/${option}"
+            break
+        done
+    done
+    ${directory}/../main.py ${path}
+else
+    ${directory}/../main.py $@
